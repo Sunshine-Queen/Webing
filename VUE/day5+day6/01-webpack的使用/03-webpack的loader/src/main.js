c4c8cad0ac2d358dd3404991f51db44b0ebe0ce4
Sunshine-Queen <1053710934@qq.com>
2021-02-06 20:48:11 +0800

    effor

--- VUE/day5+day6/01-webpack的使用/03-webpack的loader/src/main.js
+++ VUE/day5+day6/01-webpack的使用/03-webpack的loader/src/main.js
@@ -1,11 +1,18 @@
 //1.使用commonjs模块化规范
-const { add, mul } = require('./mathUtils.js')
+const { add, mul } = require('./js/mathUtils')
 
 console.log(add(20, 30));
 console.log(mul(20, 30));
 
 //2.使用ES6模块化规范
-import { name, age, height } from "./info";
+import { name, age, height } from "./js/info";
 console.log(name);
 console.log(age);
 console.log(height);
+
+//3.依赖css文件
+require('./css/normal.css')
+
+//4.依赖Less文件
+require('./css/special.less')
+document.write('<h2>你好啊。李银河</h2>')
